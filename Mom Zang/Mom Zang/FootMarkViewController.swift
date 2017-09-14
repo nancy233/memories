1ec09791208db438662cecd92e6775b4131e6d86
Nancyyyyyy <jnnancy233@gmail.com>
2017-09-14 12:09:12 -0700

    backup

--- Mom Zang/Mom Zang/FootMarkViewController.swift
+++ Mom Zang/Mom Zang/FootMarkViewController.swift
@@ -14,6 +14,7 @@ class FootMarkViewContvarler:UIViewController, MKMapViewDelegate{
     
     var selectedPlace:String?
     
+    
     @IBOutlet weak var Map: MKMapView!
     
     override func viewDidLoad() {
